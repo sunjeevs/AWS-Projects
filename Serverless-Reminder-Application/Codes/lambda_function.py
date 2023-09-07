bf01b8e08025773a757cb7953f7e9c8aeb09f838
sunjeevs <97915617+sunjeevs@users.noreply.github.com>
2023-09-08 00:04:49 +0530

    Create lambda_function.py

---
+++ Serverless-Reminder-Application/Codes/lambda_function.py
@@ -0,0 +1,17 @@
+import boto3, os, json
+
+FROM_EMAIL_ADDRESS = 'sunjeevsomu02+sending@gmail.com'
+
+ses = boto3.client('ses')
+
+def lambda_handler(event, context):
+    # Print event data to logs .. 
+    print("Received event: " + json.dumps(event))
+    # Publish message directly to email, provided by EmailOnly or EmailPar TASK
+    ses.send_email( Source=FROM_EMAIL_ADDRESS,
+        Destination={ 'ToAddresses': [ event['Input']['email'] ] }, 
+        Message={ 'Subject': {'Data': 'Schedule your meetings!'},
+            'Body': {'Text': {'Data': event['Input']['message']}}
+        }
+    )
+    return 'Success!'
