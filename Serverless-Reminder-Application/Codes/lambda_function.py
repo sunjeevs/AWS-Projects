e1e760729651d6cdd78444473234867b19c44542
sunjeevs <97915617+sunjeevs@users.noreply.github.com>
2023-09-08 00:09:11 +0530

    Update lambda_function.py

--- Serverless-Reminder-Application/Codes/lambda_function.py
+++ Serverless-Reminder-Application/Codes/lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     # Print event data to logs .. 
     print("Received event: " + json.dumps(event))
     # Publish message directly to email, provided by EmailOnly or EmailPar TASK
-    ses.send_email( Source=FROM_EMAIL_ADDRESS,
+    ses.send_email( Source=sunjeevsomu02+sending@gmail.com,
         Destination={ 'ToAddresses': [ event['Input']['email'] ] }, 
         Message={ 'Subject': {'Data': 'Schedule your meetings!'},
             'Body': {'Text': {'Data': event['Input']['message']}}
